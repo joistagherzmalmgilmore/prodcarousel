0fd3eafee30a62697a09f3e8feddaa0e94cfd8d2
joistagherzmalmgilmore <161892446+joistagherzmalmgilmore@users.noreply.github.com>
2024-03-23 09:28:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 let navLinks = document.querySelectorAll(".carousel .nav-link");
 let slides = document.querySelectorAll(".carousel .slides img");
+let overlays = document.querySelectorAll(".carousel .bar");
