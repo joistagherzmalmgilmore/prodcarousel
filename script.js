d5aea503769e62e90aa38884e7fb65ae5fbc96e6
joistagherzmalmgilmore <161892446+joistagherzmalmgilmore@users.noreply.github.com>
2024-03-24 21:28:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 let navLinks = document.querySelectorAll(".carousel .nav-link");
 let slides = document.querySelectorAll(".carousel .slides img");
 let overlays = document.querySelectorAll(".carousel .bar");
+let maxZIndex = navLinks.length;
